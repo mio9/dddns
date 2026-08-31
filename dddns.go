d12061234cd6a6ced67d058264ce3b5620b3377b
Mio Nekoya <admin@mio9.sh>
2026-08-31 18:25:24 +0900

    cli

--- dddns.go
+++ dddns.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	cli := &cli.Command{
+		Name:  "dddns",
+		Usage: "DDNS for Cloudflare (for now)",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "email",
+				Usage: "Cloudflare email",
+			},
+		},
+		Action: func(c context.Context, cmd *cli.Command) error {
+			fmt.Println("Hello, World!")
+			fmt.Println(cmd.String("email"))
+			return nil
+		},
+	}
+
+	cli.Run(context.Background(), os.Args)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mio9/dddns
 
 go 1.26.5
+
+require github.com/urfave/cli/v3 v3.11.0
